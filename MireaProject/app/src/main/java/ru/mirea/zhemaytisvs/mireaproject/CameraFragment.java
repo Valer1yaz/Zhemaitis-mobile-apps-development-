cfd9ec6e4876433060dc2dbbf2931f2b9363a8e5
Valeriya Zhemaitis <Valeriyazhmts@gmail.com>
2025-05-24 15:19:00 +0300

    lesson 4

---
+++ MireaProject/app/src/main/java/ru/mirea/zhemaytisvs/mireaproject/CameraFragment.java
@@ -0,0 +1,93 @@
+package ru.mirea.zhemaytisvs.mireaproject;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+public class CameraFragment extends Fragment {
+
+    private ImageView imageView;
+    private EditText editTextName, editTextGroup, editTextTitle, editTextContent;
+    private TextView textViewIntro, textViewFullNotice;
+    private final ActivityResultLauncher<Intent> cameraLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == requireActivity().RESULT_OK && result.getData() != null) {
+                    Bundle extras = result.getData().getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    imageView.setImageBitmap(imageBitmap);
+                } else {
+                    Toast.makeText(requireContext(), "Не удалось сделать фото", Toast.LENGTH_SHORT).show();
+                }
+            });
+    private final ActivityResultLauncher<String> permissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    launchCamera();
+                } else {
+                    Toast.makeText(requireContext(), "Для съемки фото требуется разрешение на использование камеры", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        textViewIntro = view.findViewById(R.id.textViewIntro);
+        imageView = view.findViewById(R.id.imageView);
+        editTextName = view.findViewById(R.id.editTextName);
+        editTextGroup = view.findViewById(R.id.editTextGroup);
+        editTextTitle = view.findViewById(R.id.editTextTitle);
+        editTextContent = view.findViewById(R.id.editTextContent);
+        Button buttonAddImage = view.findViewById(R.id.buttonAddImage);
+        Button buttonSave = view.findViewById(R.id.buttonSave);
+
+        buttonAddImage.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                launchCamera();
+            } else {
+                permissionLauncher.launch(Manifest.permission.CAMERA);
+            }
+        });
+
+        buttonSave.setOnClickListener(v -> {
+            String name = editTextName.getText().toString().trim();
+            String group = editTextGroup.getText().toString().trim();
+            String title = editTextTitle.getText().toString().trim();
+            String content = editTextContent.getText().toString().trim();
+
+            if (name.isEmpty() || group.isEmpty() || title.isEmpty() || content.isEmpty()) {
+                Toast.makeText(requireContext(), "Пожалуйста, заполните все поля", Toast.LENGTH_SHORT).show();
+            } else {
+                String fullNotice = "ФИО: " + name + "\n" +
+                        "Группа: " + group + "\n" +
+                        "Название заметки: " + title + "\n" +
+                        "Текст заметки: " + title;
+                textViewFullNotice.setText(fullNotice);
+                Toast.makeText(requireContext(), "Профиль сохранен", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+    private void launchCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraLauncher.launch(takePictureIntent);
+    }
+}
---
+++ MireaProject/app/src/main/java/ru/mirea/zhemaytisvs/mireaproject/MainActivity.java
@@ -0,0 +1,69 @@
+package ru.mirea.zhemaytisvs.mireaproject;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import ru.mirea.zhemaytisvs.mireaproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_content_main);
+        NavController navController = navHostFragment.getNavController();
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home,
+                R.id.nav_gallery,
+                R.id.nav_slideshow,
+                R.id.nav_data,
+                R.id.nav_webview,
+                R.id.nav_background,
+                R.id.nav_compass,
+                R.id.nav_camera,
+                R.id.nav_microphone,
+                R.id.nav_profile,
+                R.id.nav_files)
+                .setOpenableLayout(drawer)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+}
